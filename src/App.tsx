f28087a5ecffdc3e0a7f335c6dd55094c23ddd9d
Kolya-Harutyunyan-1 <kolyah@eachbase.com>
2022-07-26 20:06:33 +0400

    create useQuizGame custom-hook, fill in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import QuizGame from "./components/QuizGame";
+import useQuizGame from "./hooks/useQuizGame";
+
 const App = () => {
+  const { startTrivia, checkAnswer, handleNextQuestion } = useQuizGame();
+
   return (
     <div className="App">
-     React Quiz Game
+      <h1>Quiz Game</h1>
+      <button
+        type="button"
+        className="start-button"
+        onClick={startTrivia}
+      >
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+      <QuizGame />
+      <button
+        type="button"
+        className="next-button"
+        onClick={handleNextQuestion}
+      >
+        Next Question
+      </button>
     </div>
   );
 }
---
+++ src/hooks/useQuizGame.ts
@@ -0,0 +1,23 @@
+import React from "react";
+
+const useQuizGame = () => {
+    const startTrivia = async () => {
+        
+    };
+
+    const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+    };
+
+    const handleNextQuestion = () => {
+
+    };
+
+    return {
+        startTrivia,
+        checkAnswer,
+        handleNextQuestion,
+    };
+};
+
+export default useQuizGame;
