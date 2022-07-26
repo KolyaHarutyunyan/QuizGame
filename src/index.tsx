2748226ff99d9f27644338cb7895addf639c15e3
Kolya-Harutyunyan-1 <kolyah@eachbase.com>
2022-07-26 18:58:45 +0400

    add quiz-bg-image

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./assets/allStyles.scss";
+import "./assets/styles/allStyles.scss";
 import App from './App';
 
 const root = ReactDOM.createRoot(
